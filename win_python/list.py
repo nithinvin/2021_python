17af9409889273df1a5b5fae709bda5a6fe4f7b1
Nithin V <nithinvblr@gmail.com>
2021-11-13 11:34:33 +0530

    strings and lists

---
+++ win_python/list.py
@@ -0,0 +1,11 @@
+list1 = [ 1, 4, 9, 16, 25, 36]
+
+list2 = [ "Str1", "Str3", "Str6", "Str7", "Str4", "St455"]
+
+#print(list1[start:end:step_size])
+print(list1[1])
+print(list1[1:4])
+print(list1[0:5:2])
+print(list1[::3])
+
+print(list2)
---
+++ win_python/strings.py
@@ -0,0 +1,8 @@
+mystr = "Nithin is a teenager"
+
+#print(mystr[start:end:step_size])
+print(mystr[2])
+print(mystr[0:5])
+print(mystr[0:15:2])
+print(mystr[::2])
+
