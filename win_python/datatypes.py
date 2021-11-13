a1dcd24595f417edd6005116e1f096f32574f923
Nithin V <nithinvblr@gmail.com>
2021-11-13 12:07:08 +0530

    Python data types

---
+++ win_python/datatypes.py
@@ -0,0 +1,20 @@
+a = 123
+b = 123.12
+c = 21231231232.2131231231313131331
+d = "MyName"
+e = True
+f = [1, 2, 3]
+g = (4, 5, 6)
+h = {1:"Rainy", 2:"Summer", 3:"Winter"}
+
+print(type(a))
+print(type(b))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+
+h = 45
+
+print(type(h))
