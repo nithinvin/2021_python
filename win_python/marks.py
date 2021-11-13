b4f4675855d1329d7b387ecf4a15ee40de38a33c
Nithin Vinayagamoorthy <nithinvblr@gmail.com>
2021-11-13 09:55:11 +0530

    Python files

---
+++ win_python/marks.py
@@ -0,0 +1,8 @@
+m1= int(input("Enter marks for maths: "))
+m2= int(input("Enter marks for sst: "))
+m3= int(input("Enter marks for english: "))
+m4= int(input("Enter marks for science: "))
+m5= int(input("Enter marks for AI: "))
+
+avg = (m1 + m2 + m3 +m4 + m5)/5
+print ("Average marks is ", avg)
---
+++ win_python/square_and_cube.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number: "))
+square = number ** 2
+cube = number ** 3
+
+print("The square of the number " + str(number) + " is " + str(square))
+print("The cube of the number " + str(number) + " is " + str(cube))
