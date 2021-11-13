3f1ae3ebb74b5aaeca2e876eba7d51afdd9518a3
Nithin V <nithinvblr@gmail.com>
2021-11-13 11:06:03 +0530

    variables

--- win_python/var1.py
+++ win_python/var1.py
@@ -1,1 +1,21 @@
-var1 = 32
+import sys
+
+var1 = 45 + 33
+
+print("The number is " + str(var1))
+
+var1 = 34
+
+var1 = "MyName"
+
+var1 = False
+
+var1 = 'Test1 ' + ' Test2'
+
+print("The number is " + str(var1))
+
+Number = 127.8e-1
+
+print("Number is ", Number)
+
+
