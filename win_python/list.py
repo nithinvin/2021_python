5dc5607c7244c38d88d5264aad3fcec5e6590b7e
Nithin V <nithinvblr@gmail.com>
2021-11-13 11:45:03 +0530

    Try tuple

--- win_python/list.py
+++ win_python/list.py
@@ -1,11 +1,23 @@
-list1 = [ 1, 4, 9, 16, 25, 36]
+list1 = [ 1, 4, 9, 16, 25, 36 ]
 
 list2 = [ "Str1", "Str3", "Str6", "Str7", "Str4", "St455"]
+
+tuple1 = (1, 4, 9, 16, 25, 36)
 
 #print(list1[start:end:step_size])
 print(list1[1])
 print(list1[1:4])
 print(list1[0:5:2])
 print(list1[::3])
+
+#print(list1[2])
+#list1[2] = 100
+#print(list1[2])
+
+print(tuple1)
+#tuple1[2] = 100
+#print(tuple1)
+
+#print(list1)
 
 print(list2)
