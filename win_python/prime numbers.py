60e3cabd1b37f1f459843a96c9ad17ae8435eb24
Nithin V <nithinvblr@gmail.com>
2021-11-17 19:13:58 +0530

    prime numbers

---
+++ win_python/prime numbers.py
@@ -0,0 +1,12 @@
+num = 4
+while num<1000000:
+    j = 2
+    while j<=(num ** 0.5):
+        #print ("Check if", num, "is divisible by", j)
+        if (num%j == 0):
+            #print (num, "is not a prime number")
+            break
+        j = j + 1
+    if (j>(num ** 0.5)):
+        print (num, "is a prime number")
+    num = num + 1
