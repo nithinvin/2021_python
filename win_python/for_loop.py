20f71e3c6fb6857c651c0cfe163e682eadd1c680
Nithin V <nithinvblr@gmail.com>
2021-11-20 20:23:19 +0530

    practice programs 1

--- win_python/for_loop.py
+++ win_python/for_loop.py
@@ -3,7 +3,20 @@ fruits = ["banana", "apple", "orange"]
 for a_fruit in fruits:
     print("Current fruit is", a_fruit)
 
-numbers = [2, 3, 5, 7, 9]
+#numbers = [2, 3, 5, 7, 9]
 
-for a_number in numbers:
-    print("Current number is", a_number)
+#for a_number in [2, 3, 5, 7, 9]:
+#    print("Current number is", a_number)
+
+for my_num in list(range(2, 5)):
+    print("Current number is", my_num)
+
+if "e" in ["a", "b", "c", "d"]:
+    print("It is present")
+else:
+    print("It is NOT present")
+
+if "mumb" not in "numbers":
+    print("It is present")
+else:
+    print("It is NOT present")
---
+++ win_python/reverse numbers.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number greater than 1000: "))
+rnum = 0
+tnum = num
+while tnum > 0:
+    digit = tnum % 10
+    tnum = int(tnum/10)
+    rnum = (rnum * 10) + digit
+
+print ("The reversed number is : ", rnum)
