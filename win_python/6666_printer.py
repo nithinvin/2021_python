e8ef0cf7b3c539dd4dd1104c71cc64c60dd41897
Nithin V <nithinvblr@gmail.com>
2021-11-28 22:11:22 +0530

    ascii

---
+++ win_python/6666_printer.py
@@ -0,0 +1,7 @@
+col = 19
+row = 3
+digit = 9
+for curr_row in range(row):
+    for curr_col in range(col):
+        print(digit, end = " ")
+    print("")
---
+++ win_python/ascii.py
@@ -0,0 +1,6 @@
+#for ascii_char in list(range(65, 91)):
+#    print(ascii_char + 32, chr(ascii_char + 32))
+
+for num in range(0, 256):
+    print(chr(num))
+
